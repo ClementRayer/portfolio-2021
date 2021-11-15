396d04ff525cc056e2afabe67080d539528fb7e4
ClementRayer <clement.rayer@gmail.com>
2021-11-15 20:35:39 +0100

    Router setup, about block component ready to work

---
+++ clement-rayer-portfolio-2021-app/src/components/HomeAbout.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="about-body">
+        <div id="about-container">
+            <div id="about-border"></div>
+            <div id="about-background"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+	name: "HomeAbout"
+};
+</script>
+
+<style lang="scss">
+#about-body{
+    background: radial-gradient(
+			103.8% 103.8% at 50% 50%,
+			#2562ff 0%,
+			#ff4141 100%
+		),
+		#353131;
+	height: 100vh;
+    #about-border{
+    width: 100vw;
+    height: 100vh;
+    background: radial-gradient(103.8% 103.8% at 50% 50%, #2562FF 0%, #FF4141 100%), #353131;
+    }
+    #about-background{
+        position: absolute;
+        top: calc(100vh + 50px);
+        left: 50px;
+        width: calc(100vw - 100px);
+        height: calc(100vh - 100px);
+        background-color: #353131;
+        }
+}
+</style>
---
+++ clement-rayer-portfolio-2021-app/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <HomeName />
+        <HomeAbout />
+    </div>
+</template>
+
+<script>
+import HomeName from '../components/HomeName'
+import HomeAbout from '../components/HomeAbout'
+
+export default {
+	name: "Home",
+	components: {
+        HomeName,
+        HomeAbout
+	}
+};
+</script>
+
+<style lang="scss">
+</style>
